0c5db34c337ec4a79bf41e940b016f9e68013b80
Justin Searls <searls@gmail.com>
2019-06-25 09:31:06 -0400

    start optparsing

--- lib/refer/cli.rb
+++ lib/refer/cli.rb
@@ -2,11 +2,13 @@ require "optparse"
 
 module Refer
   class Cli
+    DEFAULT_OPTIONS = {
+      delimiter: " ",
+    }.freeze
+
     def initialize(argv)
-      @options = {}
-      OptionParser.new { |opts|
-        # TODO: have options
-      }.parse!(argv, into: @options)
+      parse_argv!(argv, @options = DEFAULT_OPTIONS.dup)
+
       @files = argv.empty? ? Dir["**/*.rb"] : argv
     end
 
@@ -15,12 +17,43 @@ module Refer
         files: @files
       )
 
-      result.tokens.each do |d|
-        next if d.hidden?
-        puts "#{d.file}:#{d.line}:#{d.column}: #{d.full_name} (#{d.type_name})"
+      result.tokens.each do |t|
+        next if t.hidden?
+        line_components = [
+          "#{t.file}:#{t.line}:#{t.column}:",
+          t.full_name.to_s,
+          "(#{t.type_name})",
+        ]
+        puts line_components.join(@options[:delimiter])
       end
 
       0
+    end
+
+    private
+
+    def parse_argv!(argv, into)
+      op = OptionParser.new
+      op.banner += " files"
+      version!(op)
+      help!(op)
+      op.on("-d", "--delimiter [DELIM]", "String separating each column (default ' ')")
+      op.on("-n", "--name [NAME]", "Name or regex pattern ")
+      op.parse!(argv, into: into)
+    end
+
+    def version!(op)
+      op.on("-v", "--version", "Prints the version") do
+        puts VERSION
+        exit 0
+      end
+    end
+
+    def help!(op)
+      op.on("-h", "--help", "Prints this help") do
+        puts op
+        exit 0
+      end
     end
   end
 end
--- lib/refer/version.rb
+++ lib/refer/version.rb
@@ -1,3 +1,3 @@
 module Refer
-  VERSION = "0.1.0"
+  VERSION = Gem::Version.new("0.0.1")
 end
