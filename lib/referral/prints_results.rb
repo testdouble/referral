9de0e467250d7778ae342c308e7318460aa6575d
Justin Searls <searls@gmail.com>
2019-06-25 18:41:18 -0400

    refactor printers into struct

--- lib/referral/prints_results.rb
+++ lib/referral/prints_results.rb
@@ -1,12 +1,24 @@
 module Referral
+  COLUMNS = {
+    location: ->(t) {
+      "#{t.file}:#{t.line}:#{t.column}:"
+    },
+    type_name: ->(t) {
+      t.type_name.to_s
+    },
+    full_name: ->(t) {
+      t.full_name.to_s
+    },
+  }
+
   class PrintsResults
     def call(result, options)
       result.tokens.each do |t|
         next if t.hidden?
         line_components = [
-          "#{t.file}:#{t.line}:#{t.column}:",
-          t.type_name.to_s,
-          t.full_name.to_s,
+          COLUMNS[:location].call(t),
+          COLUMNS[:type_name].call(t),
+          COLUMNS[:full_name].call(t),
         ]
         puts line_components.join(options[:delimiter])
       end
